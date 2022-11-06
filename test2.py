31446d0f9ad2044a7d3c27a1b82b6bf17f490781
Dan Trickey <git@trickey.io>
2022-11-06 14:32:10 +0000

    it works

--- test2.py
+++ test2.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import asyncio
 import contextlib
 import json
-from typing import Any, AsyncGenerator, TYPE_CHECKING,  Coroutine, Type
+from typing import AsyncGenerator, TYPE_CHECKING, Type
 
 import asyncio_mqtt as aiomqtt
 from pydantic import BaseModel, ValidationError, parse_raw_as
@@ -18,35 +18,34 @@ class AstoriaService:
     def __init__(self) -> None:
         self._state = AstoriaState(disks=None, process=None, tick=None)
         self._state_lock = asyncio.Lock()
-        self._tasks: set[asyncio.Future] = set()
 
     async def main(self) -> None:
         async with contextlib.AsyncExitStack() as stack:
             client = aiomqtt.Client("localhost", client_id="test")
             await stack.enter_async_context(client)
 
-            await self.register_tasks(client)
+            await self.setup(client)
+
+            tasks = set()
 
             for domain, model in DOMAIN_MAP.items():
                 manager = client.filtered_messages(f"ast/dom/{domain}")
                 messages = await stack.enter_async_context(manager)
-                self._create_task(self._handle_state_message(messages, domain, model))
+                task = asyncio.create_task(self._handle_state_message(messages, domain, model))
+                tasks.add(task)
 
             messages = await stack.enter_async_context(client.unfiltered_messages())
-            self._create_task(self._handle_unknown_message(messages))
+            task = asyncio.create_task(self._handle_unknown_message(messages))
+            tasks.add(task)
 
             await client.subscribe("ast/dom/+")
 
-            await asyncio.gather(*self._tasks)
+            await asyncio.gather(*tasks)
 
         await self._cancel_tasks()
 
-    async def register_tasks(self, client) -> None:
+    async def setup(self, client) -> None:
         pass
-
-    def _create_task(self, coro: Coroutine[Any, Any, None]) -> None:
-        task = asyncio.create_task(coro)
-        self._tasks.add(task)
 
     async def handle_state(self, state: AstoriaState) -> None:
         pass
@@ -78,7 +77,7 @@ class AstoriaService:
                     asyncio.create_task(self.handle_state(self._state))
 
     async def _cancel_tasks(self):
-        for task in self._tasks:
+        for task in asyncio.all_tasks():
             if task.done():
                 continue
             try:
@@ -102,12 +101,26 @@ class TestService(AstoriaService):
             await client.publish("ast/dom/tick", f"{{\"n\": {n}}}")
             await asyncio.sleep(1)
 
-    async def register_tasks(self, client) -> None:
-        self._create_task(self.tick(client))
+    async def forever(self) -> None:
+        while True:
+            print("yeet")
+            await asyncio.sleep(0.5)
+
+    async def setup(self, client) -> None:
+        asyncio.create_task(self.forever())
+        asyncio.create_task(self.tick(client))
 
     async def handle_state(self, state: AstoriaState) -> None:
         print(state)
+        if state["tick"] and state["tick"].n > 4000:
+            print("Time to exit!")
+            await self._cancel_tasks()
+
+
 
 if __name__ == "__main__":
     service = TestService()
-    asyncio.run(service.main())
+    try:
+        asyncio.run(service.main())
+    except asyncio.CancelledError:
+        print("Exited")
